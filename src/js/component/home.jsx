10f27361d15f6866208dfe60045ab904a88717bb
Beka Khutsishvili <157316467+bekaxuci@users.noreply.github.com>
2024-07-18 19:21:56 +0000

    First Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [activarColor, setActivarColor] = useState("");
+	const [activarBotonCambiarColor, SetActivarBotonCambiarColor] = useState("");
+
+  
+	const cambiarColor = (color) => {
+		setActivarColor(color);
+	};
+
+	const cambiarColorBoton = () => {
+		const colores = ['rojo', 'amarillo', 'verde'];
+		const colorActual = colores.indexOf(activarColor);
+		const siguienteColor = colores[(colorActual + 1) % colores.length];
+		setActivarColor(siguienteColor);
+		setActivarBotonCambiarColor(siguienteColor);
+	  };
+  
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	  <div>
+		<div className="d-flex justify-content-center">
+			<div  className="text-center bg-black" id="semaforo1"></div>
 		</div>
+		<div id="semaforo" className="text-center bg-black">
+		  <button
+			type="button"
+			id="circulos"
+			className={`button ${activarColor === 'rojo' ? 'bg-danger shine' : 'bg-danger'}`}
+			onClick={() => cambiarColor('rojo')}
+		  ></button>
+		  <button
+			type="button"
+			id="circulos"
+			className={`button ${activarColor === 'amarillo' ? 'bg-warning shine' : 'bg-warning'}`}
+			onClick={() => cambiarColor('amarillo')}
+		  ></button>
+		  <button
+			type="button"
+			id="circulos"
+			className={`button ${activarColor === 'verde' ? 'bg-success shine' : 'bg-success'}`}
+			onClick={() => cambiarColor('verde')}
+		  ></button>
+		</div>
+		<div id="cambiarcolor">
+		<button
+          type="button"
+          id="buttoncambiarcolor"
+          className="btn btn-primary"
+          onClick={cambiarColorBoton}
+        >
+          Cambiar color
+        </button>
+			
+		</div>
+	  </div>
 	);
-};
+  };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 //import react into the bundle
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 
 // include your styles into the webpack bundle
